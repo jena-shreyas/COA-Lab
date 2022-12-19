1713614812fb260017e4dd9d89b3fdcfef79e351
jena-shreyas <jenashreyas@gmail.com>
2022-12-20 00:10:14 +0530

    added CLA to Assn 3

--- Assn3/Assgn3_Grp_63/Ripple_Carry_Adder/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "/home/shreyas/Desktop/IIT KGP/5th sem/COA/Assn3/Assgn3_Grp_63/Ripple_Carry_Adder/full_adder_tb_isim_beh.exe" -prj "/home/shreyas/Desktop/IIT KGP/5th sem/COA/Assn3/Assgn3_Grp_63/Ripple_Carry_Adder/full_adder_tb_beh.prj" "work.full_adder_tb" "work.glbl" 
